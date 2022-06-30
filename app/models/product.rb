5ff1c4005299dad7fb1189d2aefd682d89fd3567
GaiaRossi <gaia.rossi@edu.unife.it>
2022-06-30 17:19:20 +0200

    mostrate le warehouse in cui si trova un prodotto

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
     has_many :review
+    has_many :product_location
     has_many :warehouse, :through => :product_location
     has_many :order, :through => :order_row
 end
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,3 +1,4 @@
 class Warehouse < ApplicationRecord
+    has_many :product_location
     has_many :product, :through => :product_location
 end
