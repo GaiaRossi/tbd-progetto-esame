5f2a42af642982d84fc57177832659fe1514e912
GaiaRossi <gaia.rossi@edu.unife.it>
2022-06-30 17:31:50 +0200

    possibilità di vedere i prodotti per ordine

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
     belongs_to :user
+    has_many :order_row
     has_many :product, :through => :order_row
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
     has_many :review
     has_many :product_location
     has_many :warehouse, :through => :product_location
+    has_many :order_row
     has_many :order, :through => :order_row
 end
