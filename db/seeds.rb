89ac3f212e7704e18b7338fe5adc69506e7d66bf
GaiaRossi <gaia.rossi@edu.unife.it>
2022-07-02 17:21:51 +0200

    creato seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+usera = User.create(username: "mario_rossi", dob: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"), email: "mariorossi@mail.com")
+userb = User.create(username: "giorgio_verdi", dob: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"), email: "gverdi@mail.com")
+
+producta = Product.create(name: "Asciugamano", description: "Per mani sempre asciutte", price: 8.99)
+productb = Product.create(name: "Asciugacapelli", description: "Per capelli sempre perfetti", price: 20.99)
+productc = Product.create(name: "Tappeto", description: "Per piedi sempre sul morbido", price: 4.99)
+
+warehousea = Warehouse.create(name: "wh-1", address: "via Verdi, 19, Milano")
+warehouseb = Warehouse.create(name: "wh-2", address: "via Bianchi, 1, Bologna")
+warehousec = Warehouse.create(name: "wh-3", address: "via Neri, 584, Pavia")
