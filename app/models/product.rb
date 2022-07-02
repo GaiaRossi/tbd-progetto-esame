a57dd01f138333c08e05bd5256f8fdeb1daafd6c
GaiaRossi <gaia.rossi@edu.unife.it>
2022-07-02 14:27:46 +0200

    aggiunta location per prodotto

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,9 @@ class Product < ApplicationRecord
     has_many :warehouse, :through => :product_location
     has_many :order_row
     has_many :order, :through => :order_row
+    accepts_nested_attributes_for :product_location, :reject_if => :quantity_blank
+
+    def quantity_blank(q)
+        q[:quantity].blank?
+    end
 end
