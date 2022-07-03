728459d78184c152fc508bf017b4f40e974360a8
GaiaRossi <gaia.rossi@edu.unife.it>
2022-07-03 09:30:39 +0200

    non puoi inserire reviews se non hai fatto ordine

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,12 +24,15 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
 
     respond_to do |format|
-      if @review.save
-        format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+      tmp = Order.past_orders(@review)
+      if tmp.present?
+        if @review.save
+          format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
+          format.json { render :show, status: :created, location: @review }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @review.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
     belongs_to :user
     has_many :order_row
     has_many :product, :through => :order_row
+
+    scope :past_orders, -> (review) { joins("INNER JOIN order_rows ON order_rows.order_id = orders.id").where("order_rows.product_id = ? AND orders.user_id = ?", review.product_id, review.user_id) }
 end
